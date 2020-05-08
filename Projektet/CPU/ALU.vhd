7bdc0a4210b8eddb7d6d504c70e0dffa30c4c474
Madsen <bmadse18@student.aau.dk>
2020-05-08 13:58:46 +0200

    ALU update
    BEMÆRK! Ved simulering aflæser du de rigtige værdier ved at indstille radix til signed.

--- Projektet/CPU/ALU.vhd
+++ Projektet/CPU/ALU.vhd
@@ -1,16 +1,17 @@
 ----------------------------------------------------------------------------------
 -- Company:				AAU
 -- Engineer: 			EIT4-415
--- Create Date:    	15/04/2020 
+-- Create Date:    	29/04/2020 
 -- Design Name: 	 	Arithmetic Logic Unit
--- Module Name:    	ALU - ALU - Behavioral
+-- Module Name:    	ALU_signed - Behavioral
 -- Description:		A signal from control unit will determine which operation
---							to execute
+--							to execute (SIGNED)
 ----------------------------------------------------------------------------------
+
 library IEEE;
-use IEEE.std_logic_1164.all;  
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.numeric_std.all;
+use IEEE.STD_LOGIC_1164.ALL;		-- Logical units
+use IEEE.STD_LOGIC_SIGNED.ALL;	-- Signed units
+use ieee.NUMERIC_STD.all;			-- Addition incode and such
 
 entity alu is
 	port (B		  : in  std_logic_vector (15 downto 0);
@@ -21,69 +22,62 @@ entity alu is
 end entity;
 
 architecture alu_arch of alu is
-
-	
-
 begin
-
-	Operations : process (A)
+	process(A,B,ALU_Sel)
+	variable Sum_sign	:	signed(16 downto 0);
+	begin
+		case(ALU_Sel) is	-- Switch case, with adresses for operations
+			when "0000" => -- ADD_AB
+				Sum_sign := signed('0' & A) + signed('0' & B);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "0001" => -- SUB_AB
+				Sum_sign := signed('0' & A) - signed('0' & B);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "0010" => -- SUB_BA
+				Sum_sign := signed('0' & B) - signed('0' & A);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "0011" => -- MUL_AB
+				Sum_sign := to_signed((to_integer(signed(A)) * to_integer(signed(B))),17);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "0100" => -- DIV_AB
+				Sum_sign := to_signed((to_integer(signed(A)) / to_integer(signed(B))),17);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "0101" => -- DIV_BA
+				Sum_sign := to_signed((to_integer(signed(B)) / to_integer(signed(A))),17);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "0110" => -- AND_AB
+				Sum_sign := signed('0' & A) AND signed('0' & B);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "0111" => -- OR_AB
+				Sum_sign := signed('0' & A) OR signed('0' & B);
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when "1000" => -- NOT_A
+				Sum_sign := signed(NOT('0' & A));
+				Result   <= std_logic_vector(Sum_sign(15 downto 0));
+			when others => -- Make sure no other case is random
+				Result   <= x"0000";
+		end case;
+	
+		-- Negative flag
+		NZVC(3) <= Sum_sign(15);
 		
-		variable Sum_uns	:	unsigned(16 downto 0);
-		
-		begin
-		
-----------------------------------------------------------------------------------
--- Sum calculation
-----------------------------------------------------------------------------------
-		if(ALU_Sel = "0000") then
-			Sum_uns := unsigned('0' & A) + unsigned('0' & B);
-			Result  <= std_logic_vector(Sum_uns(15 downto 0));
-			
-			NZVC(3) <= Sum_uns(15);
-			
-			if(Sum_uns(15 downto 0) = x"0000") then
-				NZVC(2) <= '1';
-			else
-				NZVC(2) <= '0';
-			end if;
-			
-			if((A(15)='0' and B(15)='0' and Sum_uns(15)='1') or 
-				(A(15)='1' and B(15)='1' and Sum_uns(15)='0')) then
-				NZVC(1) <= '1';
-			else
-				NZVC(1) <= '0';
-			end if;
-			
-			NZVC(0) <= Sum_uns(16);
-		elsif(ALU_Sel = "0000") then -- ADD_AB
-			Result <= A + B;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "0001") then -- SUB_AB
-			Result <= A - B;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "0010") then -- SUB_BA
-			Result <= B - A;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "0011") then -- MUL_AB
-			Result <= std_logic_vector(to_unsigned((to_integer(unsigned(A)) * to_integer(unsigned(B))),16)) ;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "0100") then -- DIV_AB
-			Result <= std_logic_vector(to_unsigned((to_integer(unsigned(A)) / to_integer(unsigned(B))),16)) ;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "0101") then -- DIV_BA
-			Result <= std_logic_vector(to_unsigned((to_integer(unsigned(B)) / to_integer(unsigned(A))),16)) ;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "0110") then -- AND_AB
-			Result <= A AND B;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "0111") then -- OR_AB
-			Result <= A OR B;
-			NZVC <= x"0";
-		elsif(ALU_Sel = "1000") then -- NOT_A
-			Result <= NOT A;
-			NZVC <= x"0";
+		-- Zero flag
+		if(Sum_sign(15 downto 0) = x"0000") then
+			NZVC(2) <= '1';
+		else
+			NZVC(2) <= '0';
 		end if;
 		
-	end process;
+		-- Overflow flag
+		if((A(15)='0' and B(15)='0' and Sum_sign(15)='1') or 
+			(A(15)='1' and B(15)='1' and Sum_sign(15)='0')) then
+			NZVC(1) <= '1';
+		else
+			NZVC(1) <= '0';
+		end if;
+		
+		-- Carry flag
+		NZVC(0) <= Sum_sign(16);
 
+	end process;		
 end architecture;
