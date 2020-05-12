8acc0533c0a97d17437f07932d8d825098fa9826
MathiasS-AAU <44232368+MathiasS-AAU@users.noreply.github.com>
2020-05-12 13:53:27 +0200

    Ny simulation til i2s

--- Systemdele[VHDL]/Testbenches/tb_i2s.vhd
+++ Systemdele[VHDL]/Testbenches/tb_i2s.vhd
@@ -23,10 +23,7 @@
                );
           END COMPONENT;
 			 
-			 --Intern I/O
-
-         SIGNAL data_in   : STD_LOGIC_VECTOR(15 downto 0) := x"0000"; -- input 16 bit signed værdi
-			signal data_out  : STD_LOGIC_VECTOR(15 downto 0) := x"0000"; -- output 16 bit signed værdi.
+			
 			
 			--i2s bus
 			Signal SCK  : STD_LOGIC := '0'; -- continuous serial clock
@@ -41,6 +38,12 @@
 			constant value2 : std_logic_vector(15 downto 0) :=x"AFAF";
 			constant value1_L : std_logic_vector(23 downto 0) :=x"FAFAAF";
 			constant value2_L : std_logic_vector(23 downto 0) :=x"AFAFFA";
+			
+			 --Intern I/O
+
+         SIGNAL data_in   : STD_LOGIC_VECTOR(15 downto 0) := x"0000"; -- input 16 bit signed værdi
+			signal data_out  : STD_LOGIC_VECTOR(15 downto 0) := Value1; -- output 16 bit signed værdi.
+			
   BEGIN
 
   -- Component Instantiation
@@ -63,23 +66,22 @@
 		wait for SCK_period/2;
 		END PrOcEsS;
   
-		data_out <= data_in;
+		--data_out <= data_in;
   --  Test Bench Statements
      tb : PROCESS
      BEGIN
-
         wait for SCK_period; -- wait until global set/reset completes
 		  WS <= '0';
 		  SD_in <= '0';
-		  
+
 		  for i in 0 to 23 loop
 			wait for SCK_period;
-			SD_in <= '1';
+			SD_in <= Value1_L(23-i);
 		  end loop;
-		  --data_out <= Value1;
+
 		  WS <= not WS;-- 1
 		  SD_in <= '1';
-		  
+		  data_out <= Value2;
 		  for i in 0 to 23 loop
 			wait for SCK_period;
 			SD_in <= '0';
@@ -87,47 +89,46 @@
 		  WS <= not WS; --0
 		  for i in 0 to 23 loop
 			wait for SCK_period;
-			SD_in <= Value1_L(23-i);
+			SD_in <= Value2_L(23-i);
 		  end loop;
-		  --data_out <= Value2;
 		  WS <= not WS;--1
 		  for i in 0 to 23 loop
 			wait for SCK_period;
 			SD_in <= '0';
 		  end loop;
-		  WS <= not WS;--0
-		  for i in 0 to 23 loop
-			wait for SCK_period;
-			SD_in <= Value2_L(23-i);
-		  end loop;
+		  --WS <= not WS;--0
+		  --for i in 0 to 23 loop
+			--wait for SCK_period;
+			--SD_in <= Value2_L(23-i);
+		  --end loop;
 		  --data_out <= Value1;
-		  WS <= not WS;--1
-		  for i in 0 to 23 loop
-			wait for SCK_period;
-			SD_in <= '0';
-		  end loop;
-		  WS <= not WS;--0
-		  for i in 0 to 23 loop
-			wait for SCK_period;
-			SD_in <= Value1_L(23-i);
-		  end loop;
+		  --WS <= not WS;--1
+		  --for i in 0 to 23 loop
+			--wait for SCK_period;
+			--SD_in <= '0';
+		  --end loop;
+		 -- WS <= not WS;--0
+		 -- for i in 0 to 23 loop
+		--	wait for SCK_period;
+		--	SD_in <= Value1_L(23-i);
+		 -- end loop;
 		  --data_out <= Value2;
-		  WS <= not WS;--1
-		  for i in 0 to 23 loop
-			wait for SCK_period;
-			SD_in <= '0';
-		  end loop;
-		  WS <= not WS;--0
-		  for i in 0 to 23 loop
-			wait for SCK_period;
-			SD_in <= Value2_L(23-i);
-		  end loop;
-		  WS <= not WS;--1
-		  for i in 0 to 23 loop
-			wait for SCK_period;
-			SD_in <= '0';
-		  end loop;
-		  WS <= not WS;--0
+		--  WS <= not WS;--1
+		--  for i in 0 to 23 loop
+		--	wait for SCK_period;
+		--	SD_in <= '0';
+		--  end loop;
+		--  WS <= not WS;--0
+		--  for i in 0 to 23 loop
+		--	wait for SCK_period;
+		--	SD_in <= Value2_L(23-i);
+		--  end loop;
+		--  WS <= not WS;--1
+		 -- for i in 0 to 23 loop
+		--	wait for SCK_period;
+		--	SD_in <= '0';
+		 -- end loop;
+		 -- WS <= not WS;--0
         -- Add user defined stimulus here
 
         wait; -- will wait forever
